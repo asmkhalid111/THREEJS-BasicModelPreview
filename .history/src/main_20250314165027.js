3203e44d601bcbfc62deac798e86e8dac7d5ea9e
asm.khalid111 <asm.khalid111@gmail.com>
2025-03-16 22:37:20 +0600

    Removed .history folder

--- .history/src/main_20250314165027.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from "three";
-
-// geometry
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
--- .history/src/main_20250314165113.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "three";
-
-// geometry
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// material
-
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
